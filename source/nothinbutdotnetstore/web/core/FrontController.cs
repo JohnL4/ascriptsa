c3beecc02ebf165b641e541b56f49b7183b6b595
John Lusk <john-training@how-hard-can-it-be.com>
2011-05-11 17:35:24 -0400

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -4,9 +4,17 @@ namespace nothinbutdotnetstore.web.core
 {
     public class FrontController : IProcessApplicationRequests
     {
+        IFindSpecificRequestHandlers registry;
+
+        public FrontController(IFindSpecificRequestHandlers registry)
+        {
+            this.registry = registry;
+        }
+
         public void process(IContainRequestInformation request)
         {
-            throw new NotImplementedException();
+            IProcessOneUniqueRequest processor = registry.get_the_command_that_can_process(request);
+            processor.run(request);
         }
     }
 }
